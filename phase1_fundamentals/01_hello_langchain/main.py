330db8de8ba980762f19edefb5d5ae55224b4197
wys <15835431945@163.com>
2026-01-22 01:16:05 +0800

    feat | 加载环境变量

--- phase1_fundamentals/01_hello_langchain/main.py
+++ phase1_fundamentals/01_hello_langchain/main.py
@@ -14,4 +14,16 @@ phase1_fundamentals.01_hello_langchain.main 的 Docstring
 
 import os
 from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+
+# 加载环境变量
+load_dotenv() # 查找 .env 文件，将其中的内容加载到 os.environ
+QWEN_API_KEY=os.getenv("QWEN_API_KEY")
+
+if not QWEN_API_KEY or QWEN_API_KEY == "your_qwen_api_key_here":
+    raise ValueError(
+        "\n请先在 .env 文件中设置有效的 QWEN_API_KEY\n"
+        "访问 https://bailian.console.aliyun.com/cn-beijing/?tab=model#/api-key 获取免费秘钥"
+    )
 
